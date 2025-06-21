0a7b645a3baf36f55dbd7a2932247ced55e8ca62
KonotopetsDmitriy <104555817+KonotopetsDmitriy@users.noreply.github.com>
2025-06-21 14:36:21 +0300

    Homework new branch

--- Program.cs
+++ Program.cs
@@ -1,79 +1,12 @@
-﻿namespace Homework_Strings_and_Symbols;
+﻿using GamePrototype.Game;
 
-class Program
+namespace GamePrototype
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine
-            ("Menu:\n" +
-             "1. Concatenate 2 strings\n" +
-             "2. Greetings to User\n" +
-             "3. Returning a string in uppercase and lowercase\n" +
-             "4. Returning first 5 symbols of the string\n" +
-             "5. Array of strings to StringBuilder\n" +
-             "6. Word replacement\n");
-        Console.Write("Enter a menu item: ");
-        switch (Console.ReadLine())
+        static void Main(string[] args)
         {
-            case "1":
-                CheckConcatenateStrings();
-                break;
-            case "2":
-                CheckGreetUser();
-                break;
-            case "3":
-                CheckStringInformation();
-                break;
-            case "4":
-                CheckFiveSymbolsOfString();
-                break;
-            case "5":
-                CheckStringsToStringBuilder();
-                break;
-            case "6":
-                CheckReplaceWord();
-                break;
-            default:
-                Console.WriteLine("Wrong input");
-                break;
+            new GameLoop().StartGame();
         }
     }
-
-    private static void CheckConcatenateStrings()
-    {
-        var concatenateStrings = new ConcatenateStrings();
-        concatenateStrings.Example();
-        concatenateStrings.UserInput();
-    }
-    private static void CheckGreetUser()
-    {
-        var greetUser = new GreetUser();
-        greetUser.Example();
-        greetUser.UserInput();
-    }
-    private static void CheckStringInformation()
-    {
-        var stringInfo = new StringInformation();
-        stringInfo.Example();
-        stringInfo.UserInput();
-    }
-    private static void CheckFiveSymbolsOfString()
-    {
-        var fiveSym = new FiveSymbolsOfString();
-        fiveSym.Example();
-        fiveSym.UserInput();
-    }
-    private static void CheckStringsToStringBuilder()
-    {
-        var srtBld = new StringsToStringBuilder();
-        srtBld.Example();
-        srtBld.UserInput();
-    }
-    private static void CheckReplaceWord()
-    {
-        var replWrd = new ReplaceWord();
-        replWrd.Example();
-        replWrd.UserInput();
-    }
 }
-    
